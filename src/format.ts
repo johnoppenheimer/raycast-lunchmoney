cfdae79abc9fd04e01a724603ab8e35ae8fef60a
Maxime Cattet <maximecattet@gmx.com>
2025-01-31 10:12:29 +0100

    style: Remove useless quotes

--- src/format.ts
+++ src/format.ts
@@ -1,4 +1,4 @@
 import * as lunchMoney from "./lunchmoney";
 
 export const getFormatedAmount = (transaction: lunchMoney.Transaction): string =>
-  `${Intl.NumberFormat("en-US", { style: "currency", currency: transaction.currency }).format(transaction.to_base)}`;
+  Intl.NumberFormat("en-US", { style: "currency", currency: transaction.currency }).format(transaction.to_base);
